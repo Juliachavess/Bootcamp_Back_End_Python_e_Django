0412e53787dab5038424e7a2b5c07cb201267c0c
Julia Chaves <juliachavess@gmail.com>
2024-07-24 17:53:34 -0300

    Add exercício 01

---
+++ Desafio01_Conceitos/Exercicios01.py
@@ -0,0 +1,17 @@
+'''Faça um Programa que peça dois números, realize as principais
+operações soma, subtração, multiplicação, divisão'''
+
+num01 = int(input('Digite o primeiro número: '))
+num02 = int(input('Digite o segundo número: '))
+
+soma = num01+num02
+print(F'O valor da soma é {soma}')
+
+sub = num01-num02
+print(F'O valor da subtração é {sub}')
+
+mult = num01*num02
+print(F'O valor da multiplicação é {mult}')
+
+divi = num01/num02
+print(F'O valor da divisão é {divi}')
